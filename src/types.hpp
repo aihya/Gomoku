35a8104c66e97dfab07e14350ab9aaccb4b4eed0
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-18 18:10:34 +0100

    added t_row - update t_board - update t_superposition

--- src/types.hpp
+++ src/types.hpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include <bits/stdc++.h>
 
+// Board rows
+typedef std::pair<std::bitset<19>, std::bitset<19>> t_row;
+
 // Bit maps of each color in the board
-typedef std::vector<std::pair<std::bitset<19>, std::bitset<19>> > t_board;
+typedef std::vector<t_row> t_board;
 
 // A vector containing the superposition of the bit masks
-typedef std::vector<int> t_superposition;
+typedef std::vector<std::bitset<19>> t_superposition;
 
 // A pair representing the coordinants of a board position
 typedef std::pair<int, int> t_coord;
